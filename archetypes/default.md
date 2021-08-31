c19211db8b38102122908fbb02fccd230a5846f6
JupiterXue <xfavor2top@gmail.com>
2021-08-31 11:09:24 +0800

    更新博客

--- archetypes/default.md
+++ archetypes/default.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ---
 title: "{{ replace .Name "-" " " | title }}"
 date: {{ .Date }}
@@ -8,16 +7,3 @@ tags:
 keywords:
     - 理论
 ---
-
-=======
----
-title: "{{ replace .Name "-" " " | title }}"
-date: {{ .Date }}
-category: tech
-tags:
-    - Go
-keywords:
-    - 理论
----
-
->>>>>>> 0cae884533b7a5e0cf3eb8be746627712e680e3e
--- deploy_blog.sh
+++ deploy_blog.sh
@@ -1,25 +1,11 @@
-#!/bin/bash
-
-myblog_dir=/root/blog      #博客网站根目录
-public=/root/blog/public   #生成的git静态网页
-msg="build site `date`"    #git commit的备注信息
-
-if [ -e $public ];then
-        cd $myblog_dir
-        rm -rf $public  #删除原有静态文件
-        hugo --theme=cactus --baseUrl="https://github.com/JupiterXue/JupiterXue.github.io" --buildDrafts     #指定主题编译成静态文件，存放在public
-        cd $public
-        git init
-        git add .
-
-        if [ $# -eq 1 ];then
-          msg = "$1"
-        fi
-        git commit -m "$msg"
-        git remote set-url origin https://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@github.com/JupiterXue/JupiterXue.github.io.git
-        git remote add origin https://github.com/JupiterXue/JupiterXue.github.io.git	#添加仓库源
-        git push -u origin master  #推送到GitHub
-        if [ $? -eq 0 ];then
-                echo "文章已更新至github！"
-        fi
+echo 编译测试
+hugo
+if [ $? -ne 0 ]; then
+    echo failed
+else
+    echo succeed
 fi
+
+git add .
+git commit -m 更新博客
+git push
