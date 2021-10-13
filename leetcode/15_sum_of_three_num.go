6326d830c63381288ce6bab84f18d86a3450d73b
JupiterXue <JupiterXue@users.noreply.github.com>
2021-10-13 16:00:15 +0000

    更新博客 798ddb6781ae229c385ffc694cf493e52771772a

---
+++ leetcode/15_sum_of_three_num.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func threeSum(nums []int) [][]int {
+	// 1. 找到所有三元组
+	// 2. 计算所有结果
+	// 3. 判断结果
+	fmt.Println(1)
+	return [][]int{}
+}
+
+func main() {
+	nums := []int{1}
+	fmt.Println(threeSum(nums))
+}
